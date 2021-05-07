6a3a232b7b91ba2c6a9957bcc9da8809a31111d0
Roniel Nunes <roniel.barbosa@ufv.br>
2021-05-07 19:59:35 -0300

    conversor funcionado

--- 1.0 - softwareConversor/Sources/conversor.c
+++ 1.0 - softwareConversor/Sources/conversor.c
@@ -5,7 +5,10 @@ void converte(char *nomeArquivo){
     FILE *arquivo = NULL;
     FILE *arquivoSaida = NULL;
     char nomeArquivoEntrada[70], nomeArquivoSaida[70];
- 
+    int numeroDeCidades;
+    int numeroCidades,verttice, distancia;
+    float xi,xj,yi,yj;
+    
     strcpy(nomeArquivoEntrada,"./arquivosOriginais/"); 
     strcat(nomeArquivoEntrada,nomeArquivo);
     strcat(nomeArquivoEntrada,".txt");
@@ -27,5 +30,35 @@ void converte(char *nomeArquivo){
     }else{
         printf("Inicializacao de arquivos realizada.\n");
     }
+ 
+    fscanf(arquivo,"%d",&numeroCidades);
+    fprintf(arquivoSaida,"%d \n", numeroCidades);
+    
+    double matriz[numeroCidades][2];
 
+    for (int i = 0; i < numeroCidades; i++)
+    {
+        fscanf(arquivo,"%d %f %f",&verttice,&xi,&yi);
+        matriz[i][0] = xi;
+        matriz[i][1] = yi;
+    }
+    
+    for (int i = 0; i < numeroCidades; i++)
+    {
+        printf("%d %f %f \n",i+1,matriz[i][0],matriz[i][1]);
+    }
+    
+    for (int i = 0; i < numeroCidades; i++)
+    {
+        for (int j = 0; j < numeroCidades; j++)
+        {
+            if(i != j){
+                distancia = sqrt(pow(matriz[i][0] - matriz[j][0],2) + pow(matriz[i][1]- matriz[j][1],2));
+                printf("Distancia de %d a %d = %d\n",i+1,j+1,distancia);
+                fprintf(arquivoSaida,"%d %d %d\n",i+1,j+1,distancia);
+            }   
+        }
+    }
+    fclose(arquivoSaida);
+    fclose(arquivo);
 }
