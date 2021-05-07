705f2a4a7101963e6882b5ce474ad08402079624
Roniel Nunes <roniel.barbosa@ufv.br>
2021-05-07 19:27:43 -0300

    Abertura e criação de arquivo testada

--- 1.0 - softwareConversor/Sources/conversor.c
+++ 1.0 - softwareConversor/Sources/conversor.c
@@ -2,20 +2,30 @@
 
 //gcc -o exec main.c ./Sources/conversor.c
 void converte(char *nomeArquivo){
-    FILE *arquivo;
-    FILE *arquivoSaida;
+    FILE *arquivo = NULL;
+    FILE *arquivoSaida = NULL;
     char nomeArquivoEntrada[70], nomeArquivoSaida[70];
  
- 
-    strcpy(nomeArquivoEntrada,"../arquivosOriginais/");
+    strcpy(nomeArquivoEntrada,"./arquivosOriginais/"); 
     strcat(nomeArquivoEntrada,nomeArquivo);
     strcat(nomeArquivoEntrada,".txt");
-    //printf("%s\n",nomeArquivoEntrada);
+    printf("%s\n",nomeArquivoEntrada);
 
-    strcpy(nomeArquivoSaida,"../arquivosSaida/");
+    strcpy(nomeArquivoSaida,"./arquivosSaida/");
     strcat(nomeArquivoSaida,nomeArquivo);
     strcat(nomeArquivoSaida,".txt");
+    printf("%s\n",nomeArquivoSaida);
 
     arquivo = fopen(nomeArquivoEntrada,"r");//Abertura do arquivo para read
     arquivoSaida = fopen(nomeArquivoSaida,"w");//Abertura do arquivo para write
+
+    if(arquivo == NULL){
+        printf("Erro na abertura do arquivo.\n");
+        return;
+    }else if(arquivoSaida == NULL){
+        printf("Erro na criacao do arquivo de escrita.\n");
+    }else{
+        printf("Inicializacao de arquivos realizada.\n");
+    }
+
 }
